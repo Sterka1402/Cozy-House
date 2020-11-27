ecfc3851f15e72b8a02eb2093a4e8b373ac0e136
Sterka1402 <kozlova.valya@gmail.com>
2020-11-27 16:01:11 +0300

    create button and hover

--- src/modules/render-pets.js
+++ src/modules/render-pets.js
@@ -1,18 +1,23 @@
 const renderPets = (pets,container) => {
 
   pets.forEach((pet) => {
-    // console.log(pet.name,pet.age, pet.description);
+    
     
     const petCard = document.createElement('div');
     petCard.innerHTML = `
     <img src='./img/${pet.picture}'>
     <h4>${pet.name}</h4>
-    <button>Learn more</button>
-    `;
+     `;
      
-     
+    const btn = document.createElement('button');
+    btn.innerHTML = `Learn more`;
+    btn.className = 'ourpets-buttons hover'; 
+
+    petCard.append(btn); 
     petCard.classList.add('ourpets-card');
     container.append(petCard);
+   
+    
   });
 
 };
--- src/pages/our-pets.js
+++ src/pages/our-pets.js
@@ -3,12 +3,6 @@ import renderPets from '../modules/render-pets';
 
 const petsContent = document.querySelector('.ourpets-content');
 
-const btn = document.createElement('button');
-btn.innerHTML = `Learn more`;
-btn.classList.add('ourpets-buttons');
-
-
-// console.log(petsContent);
 
 renderPets(pets, petsContent);
 
