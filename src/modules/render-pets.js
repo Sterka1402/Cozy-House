82b9904e211242d2a6736ca2ea578215c29352db
Sterka1402 <kozlova.valya@gmail.com>
2020-12-15 13:53:00 +0300

    create function sortPetsByName

--- src/modules/render-pets.js
+++ src/modules/render-pets.js
@@ -1,17 +1,24 @@
 const renderPets = (pets, container) => {
+  console.log(pets);
   if (!container) return;
   pets.forEach((pet) => {
     const petCard = document.createElement('div');
-    petCard.innerHTML = `
-    <img src='./img/${pet.picture}'>
-    <h4>${pet.name}</h4>
-     `;
-    const btn = document.createElement('button');
-    btn.innerHTML = 'Learn more';
-    btn.className = 'ourpets-buttons hover';
-    petCard.append(btn);
-    petCard.classList.add('ourpets-card');
-    container.append(petCard);
+    if (pets === []) {
+      petCard.innerHTML = "";
+      // container.value = 0;
+      return;
+    } else{
+      petCard.innerHTML = `
+      <img src='./img/${pet.picture}'>
+      <h4>${pet.name}</h4>
+      `;
+      const btn = document.createElement('button');
+      btn.innerHTML = 'Learn more';
+      btn.className = 'ourpets-buttons hover';
+      petCard.append(btn);
+      petCard.classList.add('ourpets-card');
+      container.append(petCard);
+    }
   });
 };
 export default renderPets;
--- src/pages/our-pets.js
+++ src/pages/our-pets.js
@@ -2,6 +2,24 @@ import pets from '../modules/pets-data';
 import renderPets from '../modules/render-pets';
 
 window.addEventListener('load', () => {
-  const petsContent = document.querySelector('.ourpets-content');
-  renderPets(pets, petsContent);
+    const petsContent = document.querySelector('.ourpets-content');
+    const sortLink = document.querySelector('.sort-pets');
+  
+    function sortPetsByName(pets,petsContent)  {
+      for (let i = 0; i < pets.length;i++){
+      // console.table(pets[i].name);
+      }
+       const sortPets = pets.sort(function(a,b) {
+         const aName = a.name;
+         const bName = b.name;
+        return aName > bName ? 1: -1;
+      });
+      console.log(sortPets);
+      renderPets([],petsContent);
+      renderPets(sortPets, petsContent);
+    }
+
+  
+    renderPets(pets, petsContent);
+    sortLink.addEventListener('click',(e) => sortPetsByName(pets));
 });
